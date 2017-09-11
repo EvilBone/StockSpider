1e3108019451c69a7251b137f0b9c98c8c12912a
Jie_Hu <bonehj@163.com>
2017-09-11 18:05:47 +0800

    J

--- stock/admin.py
+++ stock/admin.py
@@ -5,5 +5,10 @@ from stock.models import Stock
 class StockAdmin(admin.ModelAdmin):
     fields = ('stock_code','stock_name','stock_price','stock_lastPrice','stock_openprice','stock_maxprice','stock_minprice','stock_dealqty','stock_dealmoney')
     list_display = ('stock_code','stock_name','stock_price','stock_lastPrice','stock_openprice','stock_maxprice','stock_minprice','stock_dealqty','stock_dealmoney')
+    # list_filter = ('stock_price',)
+    search_fields = ('stock_code','stock_name')
+    # list_per_page设置每页显示多少条记录，默认是100条
+    list_per_page = 10
+    #list_editable = ('stock_price', 'stock_name')
 
 admin.site.register(Stock,StockAdmin)
